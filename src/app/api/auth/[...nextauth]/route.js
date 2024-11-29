c4d9e51ab7d14aaafc10791931cef2d9ff9913f8
MdShuvo18 <mdks33881@gmail.com>
2024-11-29 21:15:57 +0600

    get nextAuth session data

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,23 @@
 import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 const handler = NextAuth({
     session: {
-
+        strategy: "jwt"
     },
     providers: [
-        {
+        CredentialsProvider({
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "example@example.com" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if (!credentials) {
+                    return null;
+                }
+                return true;
+            }
 
-        }
+        })
     ]
 })
 
